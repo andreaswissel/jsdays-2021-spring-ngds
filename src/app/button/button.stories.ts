2266c28f852eb42640a40ef4500e2fdb8bee7f5b
Andreas Wissel <github@kaeku.io>
2021-03-22 20:49:05 +0100

    lab-4

--- src/app/button/button.stories.ts
+++ src/app/button/button.stories.ts
@@ -13,4 +13,20 @@ export const Default = Template.bind({});
 
 Default.args = {
   label: 'Submit',
+  bgColor: '#0085FC',
+  fontSize: 13,
+};
+
+Default.argTypes = {
+  label: {
+    name: 'Label',
+  },
+  bgColor: {
+    name: 'Background Color',
+    control: 'color',
+  },
+  fontSize: {
+    name: 'Font Size',
+    control: { type: 'range', min: 8, max: 20 },
+  },
 };
